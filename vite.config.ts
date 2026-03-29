ebee2e47d08c01e69fe123105743e28f9a42b55f
santiago <a01255853@tec.mx>
2026-03-28 20:18:08 -0700

    fix: configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/croak_webpage/",
   plugins: [
     react(),
     tailwindcss(),
